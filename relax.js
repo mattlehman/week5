46d5e997bcf7dbee079060b3b286c9e2bc366a20
jserrao <bigbadserrao@gmail.com>
2016-07-29 01:28:58 -0400

    Correction for main JS file

--- relax.js
+++ relax.js
@@ -1,21 +1,33 @@
 
 
 $(document).ready(function() {
+// Your attempt
   if $('.readmore').click(function(){
     $('#show-this-on-click').slideDown("slow");
       $('.readmore').hide(500);
       $('.readless').show(500);
-    } 
+    }
   else {
     $('#show-this-on-click').hide();
   }
   });
 
+// Try to code the first click function with the instructions together
+// This way it executes in one pass of the function, both showing and hiding what is necessary at the same time.
+// See if you can fix the other functions up to match this one.
+$('.readmore').click(function(){
+  $('#show-this-on-click').slideDown();
+  $('.readmore').hide();
+  $('.readless').show();
+  event.preventDefault();
+});
+
+
   if $('.readless').click(function(){
     $('#show-this-on-click').slideUp('slow');
       $('.readless').hide();
       $('readmore').show();
-  } 
+  }
   });
   else {
     $('#show-this-on-click').show(500);
